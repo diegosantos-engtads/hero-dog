05f8bc4bcbe3266ef52df8ecf1945898238e0810
diegosantos-engtads <diegosantos.engtads@gmail.com>
2025-02-22 18:11:13 -0300

    Add About section with responsive content and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { About } from "./_components/about";
 import { Hero } from "./_components/hero";
 
 export default function Home() {
   return(
     <main>
-      <h1><Hero /></h1>
+      <Hero />
+      <About />
     </main>
   )
 }
